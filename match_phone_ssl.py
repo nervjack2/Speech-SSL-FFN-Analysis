bc09888bf79ba2903260ed22938b04d4ff9c0463
林子權 <nervjack2@login.speech>
2024-05-28 16:51:45 +0800

    add visualize

--- match_phone_ssl.py
+++ match_phone_ssl.py
@@ -9,7 +9,7 @@ from melhubert.model import MelHuBERTModel, MelHuBERTConfig
 from data import DataProcessor
 from tools import get_monophone_end, get_monophone_mid, get_monophone_start
 
-def main(model_pth, mfa_json, save_pth, fp, mean_std_pth, data_pth, phone_type, extra_class, tsv_pth):
+def main(model_pth, km_pth, save_pth, mean_std_pth, data_pth, extra_class, tsv_pth):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     # Load upstream model 
     all_states = torch.load(model_pth, map_location="cpu")
@@ -22,23 +22,14 @@ def main(model_pth, mfa_json, save_pth, fp, mean_std_pth, data_pth, phone_type, 
     state_dict = all_states["model"]
     upstream_model.load_state_dict(state_dict)
     upstream_model.eval() 
-    
     # Load mean std 
-    dp = DataProcessor(mean_std_pth, device, fp)
+    dp = DataProcessor(mean_std_pth, device, fp=20)
     # Wav paths 
     wav_pths = list(glob.glob(data_pth+"**/*.flac", recursive=True))
-    
-    # =====Change to unsupervised clusters==========
-    # Load phoneme force align result 
-    with open(mfa_json, 'r') as fp:
-        mfa = json.load(fp)
-    print(f"Load MFA result from {len(mfa)} utterances")
-    # =====Change to unsupervised clusters==========
 
     NLAYER = upstream_config['encoder_layers']
     D = upstream_config['encoder_ffn_embed_dim']
     D = D if type(D) == list else [D for i in range(NLAYER)]
-    N_phone = 41
 
     # Load tsv path
     key_tsv_list = [] 
@@ -49,8 +40,19 @@ def main(model_pth, mfa_json, save_pth, fp, mean_std_pth, data_pth, phone_type, 
             key = pth.split('/')[-1].split('.')[0]
             key_tsv_list.append(key)
 
+    km_dict = {}
+    max_c = 0
+    with open(km_pth, 'r') as fp:
+        for key, x in zip(key_tsv_list, fp):
+            x = list(map(int, x.split(' ')))
+            km_dict[key] = x 
+            max_c = max(max_c, max(x)) 
+
+    N_cluster = max_c+1
+    print(f"Load MFA result from {len(km_dict)} utterances")
+
     N_list = {
-        'ivector-2c': N_phone*2
+        'ivector-2c': N_cluster*2
     }
     N = N_list[extra_class]
 
@@ -64,16 +66,9 @@ def main(model_pth, mfa_json, save_pth, fp, mean_std_pth, data_pth, phone_type, 
                 
     for pth in tqdm(wav_pths): 
         key = pth.split('/')[-1].split('.')[0]
-        phoneme = mfa[key]
+        clusters = km_dict[key]
         if extra_class == 'ivector-2c':
             g = ivector_dict[key] 
-        if phone_type == 'end-phone':
-            check_idx = get_monophone_end(phoneme)
-        elif phone_type == 'mid-phone':
-            check_idx = get_monophone_mid(phoneme)
-        elif phone_type == 'start-phone':
-            check_idx = get_monophone_start(phoneme)
-        check_phone = [phoneme[idx] for idx in check_idx]
         # Forward models to get FFC layer results 
         mel_input, pad_mask = dp.prepare_data(pth)
     
@@ -81,17 +76,15 @@ def main(model_pth, mfa_json, save_pth, fp, mean_std_pth, data_pth, phone_type, 
             out = upstream_model(mel_input, pad_mask, get_hidden=True, no_pred=True)
         fc_results = out[7]
         for layer_idx, (fc1, fc2) in enumerate(fc_results):
-            check_keys = fc1.squeeze(1)[check_idx,:]
-            tau = round(D[layer_idx]*0.01)
-            for k in range(len(check_idx)):
-                keys = torch.abs(check_keys[k]) # D 
-                assert D[layer_idx] == len(keys)
-                p = check_phone[k] 
-                _, topk_indices = torch.topk(keys, tau)
-                topk_indices = topk_indices.cpu()
+            check_keys = fc1.squeeze(1)
+            tau = round(D[layer_idx] * 0.01)
+            assert all(D[layer_idx] == len(keys) for keys in check_keys)
+            topk_indices = torch.topk(torch.abs(check_keys), tau, dim=1).indices.cpu()
+            for k, indices in enumerate(topk_indices):
+                c = clusters[k]
                 if extra_class == 'ivector-2c':
-                    record[layer_idx][g*N_phone+p, topk_indices] += 1 
-                    record_n[layer_idx][g*N_phone+p] += 1 
+                    record[layer_idx][g*N_cluster+c, indices] += 1
+                    record_n[layer_idx][g*N_cluster+c] += 1
 
     for idx in range(12):
         for pidx in range(N):
@@ -105,12 +98,10 @@ def main(model_pth, mfa_json, save_pth, fp, mean_std_pth, data_pth, phone_type, 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--model-pth', help='Model path')
-    parser.add_argument('-f', '--mfa-json', help='Force aligning result')  
+    parser.add_argument('-k', '--km-pth', help='Kmeans clustering result')  
     parser.add_argument('-s', '--save-pth', help='Result save path')
-    parser.add_argument('-p', '--fp', type=int, help='Frame period', choices=[10, 20])
     parser.add_argument('-x', '--mean-std-pth', help='Mean std path')
     parser.add_argument('-d', '--data-pth', help='Dataset directory')
-    parser.add_argument('-t', '--phone-type', help='Phone type', choices=['end-phone', 'mid-phone', 'start-phone'])
     parser.add_argument('-c', '--extra-class', choices=['ivector-2c'])
     parser.add_argument('-v', '--tsv-pth', help='.tsv path')
     args = parser.parse_args()
--- visualize_phone.py
+++ visualize_phone.py
@@ -6,72 +6,25 @@ import matplotlib.pyplot as plt
 from tools import sort_by_same_phone, sort_voiced_unvoiced
 from sklearn.manifold import MDS
 
-def draw_intra_phone(data, save_pth, phone_idx, phone_name, layer):
-    _, D = data[layer-1].shape
-    random_baseline = round(D*0.01)/D
-    v_data = data[layer-1][phone_idx,:]
-    v_data = -np.sort(-v_data, axis=-1)
-    n_dim = v_data.shape[-1]
-    n_phone = len(phone_idx)
-    fig, axs = plt.subplots(1, n_phone, figsize=(10,4))
-    for i in range(n_phone):
-        axs[i].bar(range(n_dim), v_data[i])
-        axs[i].title.set_text(phone_name[i])
-        axs[i].axhline(y=random_baseline, color='r', linestyle='-', linewidth=1)
-    plt.savefig(save_pth, bbox_inches='tight', dpi=200)
-
-def draw_keys_activated_vector_demo(data, save_pth, phone_idx, phone_name, layer, per_phone, num_type):
-    v_data = data[layer-1][phone_idx,:]
-    n_phone = len(phone_idx)
-    D = v_data.shape[-1]
-    # Sort index by its matching probability
-    indices = [[i for i in range(D)] for i in range(n_phone)]
-    for i in range(n_phone):
-        indices[i] = sorted(indices[i], key=lambda x: v_data[i][x], reverse=True)[:per_phone]
-    keys = {}
-    idx = 0
-    key_visualize_idx = []
-    for i in range(n_phone):
-        type_v_idx = []
-        for j in range(per_phone):
-            if indices[i][j] not in keys:
-                keys[indices[i][j]] = idx 
-                idx += 1 
-            type_v_idx.append(keys[indices[i][j]])
-        key_visualize_idx.append(type_v_idx)
-    print(f'{len(keys)} activate keys over {n_phone} phoneme')
-    v_data = np.zeros((n_phone, len(keys)))
-    for i in range(n_phone):
-        v_data[i][key_visualize_idx[i]] = 1 
-    
-    plt.imshow(v_data, cmap='gray')
-    plt.xlabel('keys')
-    plt.ylabel('phone')
-    plt.yticks(range(n_phone), phone_name)
-    acc = 0
-    for i in range(len(num_type)-1):
-        plt.axhline(acc+num_type[i]-0.5, c='red')
-        acc += num_type[i]
-    plt.savefig(save_pth, bbox_inches='tight', dpi=200)
-
-def draw_mds_phone_type(data, save_pth, phone_idx, phone_name, layer, num_type):
-    v_data = data[layer-1][phone_idx,:]
-    n_phone = len(phone_idx)
+def draw_mds_ivector(data, save_pth, layer):
+    v_data = data[layer-1]
+    N_cluster = v_data.shape[0]//2
+    N_data = v_data.shape[0]
     D = v_data.shape[-1]
     random_baseline = round(D*0.01)/D
     # See how many phones have matching probability over random_baseline
-    num_dim = [0 for i in range(n_phone)]
-    for i in range(n_phone):
+    num_dim = [0 for i in range(N_data)]
+    for i in range(N_data):
         num_dim_meaningful = np.sum(v_data[i] > random_baseline)
         num_dim[i] = num_dim_meaningful
     # Sort index by its matching probability
-    indices = [[i for i in range(D)] for i in range(n_phone)]
-    for i in range(n_phone):
+    indices = [[i for i in range(D)] for i in range(N_data)]
+    for i in range(N_data):
         indices[i] = sorted(indices[i], key=lambda x: v_data[i][x], reverse=True)[:num_dim[i]]
     keys = {}
     idx = 0
     key_visualize_idx = []
-    for i in range(n_phone):
+    for i in range(N_data):
         type_v_idx = []
         nd = len(indices[i])
         for j in range(nd):
@@ -80,269 +33,38 @@ def draw_mds_phone_type(data, save_pth, phone_idx, phone_name, layer, num_type):
                 idx += 1 
             type_v_idx.append(keys[indices[i][j]])
         key_visualize_idx.append(type_v_idx)
-    print(f'{len(keys)} activate keys over {n_phone} phoneme')
-    v_data = np.zeros((n_phone, len(keys)))
-    for i in range(n_phone):
-        v_data[i][key_visualize_idx[i]] = 1 
-    # Multiscale scaling
-    mds = MDS(n_components=2, random_state=0)
-    v_data_2d = mds.fit_transform(v_data)
-    color = ['red', 'blue', 'green']
-    label = ['vowels', 'voiced-consonants', 'unvoiced-consonants']
-    acc = 0 
-    for idx, n in enumerate(num_type):
-        plt.scatter(v_data_2d[acc:acc+n,0], v_data_2d[acc:acc+n,1], c=color[idx], label=label[idx])
-        acc += n 
-    for idx, name in enumerate(phone_name):
-        plt.annotate(name, (v_data_2d[idx,0],v_data_2d[idx,1]))
-    plt.legend(loc='upper right', fontsize=6)
-    plt.axis('off')
-    plt.title(f'Layer {layer}')
-    plt.savefig(save_pth, bbox_inches='tight', dpi=200)
-
-def draw_mds_gender(data, save_pth, phone_name, layer, num_type, phone_idx):
-    NPHONE = data[layer-1].shape[0]//2
-    v_datas = []
-    for i in range(2):
-        idx = [NPHONE*i+x for x in phone_idx]
-        v_datas.append(data[layer-1][idx,:])
-    v_data = np.stack(v_datas, axis=0)
-    v_data = v_data.reshape(-1, v_data.shape[-1])
-    n_phone = len(phone_name)
-    n_data = 2*n_phone
-    D = v_data.shape[-1]
-    random_baseline = round(D*0.01)/D
-    # See how many phones have matching probability over random_baseline
-    num_dim = [0 for i in range(n_data)]
-    for i in range(n_data):
-        num_dim_meaningful = np.sum(v_data[i] > random_baseline)
-        num_dim[i] = num_dim_meaningful
-    # Sort index by its matching probability
-    indices = [[i for i in range(D)] for i in range(n_data)]
-    for i in range(n_data):
-        indices[i] = sorted(indices[i], key=lambda x: v_data[i][x], reverse=True)[:num_dim[i]]
-    keys = {}
-    idx = 0
-    key_visualize_idx = []
-    for i in range(n_data):
-        type_v_idx = []
-        nd = len(indices[i])
-        for j in range(nd):
-            if indices[i][j] not in keys:
-                keys[indices[i][j]] = idx 
-                idx += 1 
-            type_v_idx.append(keys[indices[i][j]])
-        key_visualize_idx.append(type_v_idx)
-    print(f'{len(keys)} activate keys over {n_phone} phoneme')
-    v_data = np.zeros((n_data, len(keys)))
-    for i in range(n_data):
+    print(f'{len(keys)} activate keys over {N_cluster} clusters')
+    v_data = np.zeros((N_data, len(keys)))
+    for i in range(N_data):
         v_data[i][key_visualize_idx[i]] = 1 
     # Multiscale scaling
     mds = MDS(n_components=2, random_state=0)
     v_data_2d = mds.fit_transform(v_data)
     color = ['red', 'blue']
-    label = ['male', 'female']
+    label = ['ivector-1', 'ivector-2']
 
     for idx in range(2):
-        plt.scatter(v_data_2d[idx*n_phone:(idx+1)*n_phone,0], v_data_2d[idx*n_phone:(idx+1)*n_phone,1], c=color[idx], label=label[idx])
-
-    for idx, name in enumerate(phone_name):
-        plt.annotate(name, (v_data_2d[idx,0],v_data_2d[idx,1]))
-        plt.annotate(name, (v_data_2d[n_phone+idx,0],v_data_2d[n_phone+idx,1]))
+        plt.scatter(v_data_2d[idx*N_cluster:(idx+1)*N_cluster,0], v_data_2d[idx*N_cluster:(idx+1)*N_cluster,1], c=color[idx], label=label[idx])
     
     plt.legend(loc='upper right', fontsize=6)
     plt.axis('off')
-    plt.title(f'Layer {layer}')
+    # plt.title(f'Layer {layer}')
     plt.savefig(save_pth, bbox_inches='tight', dpi=200)
 
-def draw_mds_duration(data, save_pth, phone_name, layer, num_type, label, phone_idx):
-    NPHONE = data[layer-1].shape[0]//3
-    v_datas = []
-    for i in range(3):
-        idx = [NPHONE*i+x for x in phone_idx]
-        v_datas.append(data[layer-1][idx,:])
-    v_data = np.stack(v_datas, axis=0)
-    v_data = v_data.reshape(-1, v_data.shape[-1])
-    n_phone = len(phone_name)
-    n_data = 3*n_phone
-    D = v_data.shape[-1]
-    random_baseline = round(D*0.01)/D
-    # See how many phones have matching probability over random_baseline
-    num_dim = [0 for i in range(n_data)]
-    for i in range(n_data):
-        num_dim_meaningful = np.sum(v_data[i] > random_baseline)
-        num_dim[i] = num_dim_meaningful
-    # Sort index by its matching probability
-    indices = [[i for i in range(D)] for i in range(n_data)]
-    for i in range(n_data):
-        indices[i] = sorted(indices[i], key=lambda x: v_data[i][x], reverse=True)[:num_dim[i]]
-    keys = {}
-    idx = 0
-    key_visualize_idx = []
-    for i in range(n_data):
-        type_v_idx = []
-        nd = len(indices[i])
-        for j in range(nd):
-            if indices[i][j] not in keys:
-                keys[indices[i][j]] = idx 
-                idx += 1 
-            type_v_idx.append(keys[indices[i][j]])
-        key_visualize_idx.append(type_v_idx)
-    print(f'{len(keys)} activate keys over {n_phone} phoneme')
-    v_data = np.zeros((n_data, len(keys)))
-    for i in range(n_data):
-        v_data[i][key_visualize_idx[i]] = 1 
-    # Multiscale scaling
-    mds = MDS(n_components=2, random_state=0)
-    v_data_2d = mds.fit_transform(v_data)
-    color = ['red', 'blue', 'green']
-    for idx in range(3):
-        plt.scatter(v_data_2d[idx*n_phone:(idx+1)*n_phone,0], v_data_2d[idx*n_phone:(idx+1)*n_phone,1], c=color[idx], label=label[idx])
-    for idx, name in enumerate(phone_name):
-        plt.annotate(name, (v_data_2d[idx,0],v_data_2d[idx,1]))
-        plt.annotate(name, (v_data_2d[n_phone+idx,0],v_data_2d[n_phone+idx,1]))
-        plt.annotate(name, (v_data_2d[n_phone*2+idx,0],v_data_2d[n_phone*2+idx,1]))
-    plt.legend(loc='upper right', fontsize=6)
-    plt.axis('off')
-    plt.title(f'Layer {layer}')
-    plt.savefig(save_pth, bbox_inches='tight', dpi=200)
 
-def draw_mds_pitch(data, save_pth, phone_name, layer, num_type, label, phone_idx):
-    NPHONE = data[layer-1].shape[0]//3
-    v_datas = []
-    for i in range(3):
-        idx = [NPHONE*i+x for x in phone_idx]
-        v_datas.append(data[layer-1][idx,:])
-    v_data = np.stack(v_datas, axis=0)
-    v_data = v_data.reshape(-1, v_data.shape[-1])
-    n_phone = len(phone_name)
-    n_data = 3*n_phone
-    D = v_data.shape[-1]
-    random_baseline = round(D*0.01)/D
-    # See how many phones have matching probability over random_baseline
-    num_dim = [0 for i in range(n_data)]
-    for i in range(n_data):
-        num_dim_meaningful = np.sum(v_data[i] > random_baseline)
-        num_dim[i] = num_dim_meaningful
-    # Sort index by its matching probability
-    indices = [[i for i in range(D)] for i in range(n_data)]
-    for i in range(n_data):
-        indices[i] = sorted(indices[i], key=lambda x: v_data[i][x], reverse=True)[:num_dim[i]]
-    keys = {}
-    idx = 0
-    key_visualize_idx = []
-    for i in range(n_data):
-        type_v_idx = []
-        nd = len(indices[i])
-        for j in range(nd):
-            if indices[i][j] not in keys:
-                keys[indices[i][j]] = idx 
-                idx += 1 
-            type_v_idx.append(keys[indices[i][j]])
-        key_visualize_idx.append(type_v_idx)
-    print(f'{len(keys)} activate keys over {n_phone} phoneme')
-    v_data = np.zeros((n_data, len(keys)))
-    for i in range(n_data):
-        v_data[i][key_visualize_idx[i]] = 1 
-    # Multiscale scaling
-    mds = MDS(n_components=2, random_state=0)
-    v_data_2d = mds.fit_transform(v_data)
-    color = ['red', 'blue', 'green', 'yellow']
-    for idx in range(3):
-        plt.scatter(v_data_2d[idx*n_phone:(idx+1)*n_phone,0], v_data_2d[idx*n_phone:(idx+1)*n_phone,1], c=color[idx], label=label[idx])
-    for idx, name in enumerate(phone_name):
-        plt.annotate(name, (v_data_2d[idx,0],v_data_2d[idx,1]))
-        plt.annotate(name, (v_data_2d[n_phone+idx,0],v_data_2d[n_phone+idx,1]))
-        plt.annotate(name, (v_data_2d[n_phone*2+idx,0],v_data_2d[n_phone*2+idx,1]))
-    plt.legend(loc='upper right', fontsize=6)
-    plt.axis('off')
-    plt.title(f'Layer {layer}')
-    plt.savefig(save_pth, bbox_inches='tight', dpi=200)
-
-def main(pkl_pth, save_pth, phone_label_pth, mode, layer_n):
+def main(pkl_pth, save_pth, mode, layer_n):
     with open(pkl_pth, 'rb') as fp:
         data = pickle.load(fp) 
 
-    with open(phone_label_pth, 'r') as fp:
-        phone_label = json.load(fp)
-    sort_phone = sorted(phone_label, key=lambda x: phone_label[x][1], reverse=True)
-    sort_phone_same = sort_by_same_phone(sort_phone)
-    sort_phone_unvoiced, num_type = sort_voiced_unvoiced(sort_phone_same)
-    print(sort_phone_unvoiced, num_type)
-
-    n_phone = len(sort_phone)
-
-    if mode == 'intra-phone':
-        # Hyperparameters
-        # =================
-        layer = layer_n
-        # =================
-        phone_name = [sort_phone[0], sort_phone[n_phone//2], sort_phone[-5]]
-        print(f"Visualize phone {' '.join(phone_name)} with occurance {' '.join([str(phone_label[n][1]) for n in phone_name])} respectively")
-        phone_idx = [phone_label[x][0] for x in phone_name]
-        draw_intra_phone(data, save_pth, phone_idx, phone_name, layer)
-    elif mode == 'mds-phone-type':
-        # Hyperparameters
-        # =================
-        layer = layer_n
-        # =================
-        phone_name = sort_phone_unvoiced
-        phone_idx = [phone_label[x][0] for x in phone_name]
-        draw_mds_phone_type(data, save_pth, phone_idx, phone_name, layer, num_type)
-    elif mode == 'mds-gender':
-        # Hyperparameters
-        # =================
-        layer = layer_n
-        # =================
-        phone_name = sort_phone_unvoiced
-        phone_idx = [phone_label[x][0] for x in phone_name]
-        draw_mds_gender(data, save_pth, phone_name, layer, num_type, phone_idx)
-    elif mode == 'mds-duration':
-        # Hyperparameters
-        # =================
-        layer = layer_n
-        # Extreme 40ms 140ms
-        # label = ['<40', '40-140', '>140']
-        # Normal 60ms 100ms
-        label = ['<60ms', '60-100ms', '>100ms']
-        # =================
-        phone_name = sort_phone_unvoiced
-        phone_idx = [phone_label[x][0] for x in phone_name]
-        draw_mds_duration(data, save_pth, phone_name, layer, num_type, label, phone_idx)
-    elif mode == 'mds-pitch':
-        # Hyperparameters
-        # =================
-        layer = layer_n
-        # Extreme 95.80838323353294, 222.91946630147046
-        # label = ['<95.81', '95.81-222.92', '>222.92']
-        # Normal 129.03225806451613, 179.77528089887642
-        label = ['<129.03Hz', '129.03-179.78Hz', '>179.78Hz']
-        # =================
-        phone_name = sort_phone_unvoiced
-        phone_idx = [phone_label[x][0] for x in phone_name]
-        draw_mds_pitch(data, save_pth, phone_name, layer, num_type, label, phone_idx)
-    elif mode == 'keys-activated-vector-demo': 
-        # Hyperparameters
-        # =================
-        layer = layer_n
-        per_phone = 5
-        # phone_name = ['AH0','AH1','AH2','AO1','AO2','AO0','D','B','G','T','S','F']
-        phone_name = sort_phone_unvoiced
-        # num_type = [6,3,3]
-        # =================
-        phone_idx = [phone_label[x][0] for x in phone_name]
-        draw_keys_activated_vector_demo(data, save_pth, phone_idx, phone_name, layer, per_phone, num_type)
+    if mode == 'mds-ivector':
+        draw_mds_ivector(data, save_pth, layer_n)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--pkl-pth', help='Data .pkl path')
     parser.add_argument('-s', '--save-pth', help='Save path')
-    parser.add_argument('-p', '--phone-label-pth', help='Phoneme lable path')
     parser.add_argument('-m', '--mode', help='Drawing figure mode', 
-                    choices=['intra-phone', 
-                            'mds-phone-type', 'mds-gender',
-                            'mds-duration', 'mds-pitch',
-                            'keys-activated-vector-demo'])
+                    choices=['mds-ivector'])
     parser.add_argument('-l', '--layer-n', help='Layer index', type=int)
     args = parser.parse_args()
     main(**vars(args))
